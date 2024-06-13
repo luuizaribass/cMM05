7c8fc80d265b742a99ffc9e4cabd24e48a0b7af9
Maria Luiza Ribas <luuiza.ribass@gmail.com>
2024-06-13 16:48:52 -0300

    ,
    ,

---
+++ exrc/QuintaLista/QuintaLista.cpp
@@ -0,0 +1,102 @@
+//#include <iostream>
+//#include <locale>
+
+
+//using namespace std;
+//int main() {
+//	locale::global(locale("pt_BR.UTF-8"));
+//
+//	int numero = 1;
+//
+//	while (numero <= 10) {
+//		cout << numero << "\n";
+//		numero += 1;
+//	}
+//}
+
+
+//using namespace std;
+//int main() {
+//	locale::global(locale("pt_BR.UTF-8"));
+//
+//	int tab = 1;
+//	int numero;
+//	cout << "Informe um número ";
+//	cin >> numero;
+//
+//	do {
+//		int resultado = numero * tab;
+//		cout << numero << " x " << tab << " = " << resultado << "\n";
+//		tab += 1;
+//	} while (tab <= 10);
+//}
+
+
+//using namespace std;
+//int main() {
+//	locale::global(locale("pt_BR.UTF-8"));
+//
+//	for (int i = 0; i <= 50; i+=5) {
+//		cout << i << "\n";
+//	}
+//}
+
+
+//using namespace std;
+//int main() {
+//	locale::global(locale("pt_BR.UTF-8"));
+//
+//	int quant = 1;
+//	
+//	do{
+//		int numero;
+//		cout << "Informe um número ";
+//		cin >> numero;
+//
+//		if (numero > 100) {
+//			cout << "Número especial \n";
+//		}
+//		else {
+//			cout << "Número comum \n";
+//		}
+//
+//		quant += 1;
+//	} while (quant <= 5);
+//
+//}
+
+
+//using namespace std;
+//int main() {
+//	locale::global(locale("pt_BR.UTF-8"));
+//
+//	int numero = 1;
+//
+//	while (numero <= 31) {
+//		cout << numero << "\n";
+//		numero += 2;
+//	}
+//}
+
+	
+//using namespace std;
+//int main(){
+//	locale::global(locale("pt_BR.UTF-8"));
+//
+//	for (int i = 1; i <= 5; i++) {
+//		int numero;
+//		cout << "Informe um número ";
+//		cin >> numero;
+//
+//		if (numero > 0) {
+//			cout << "O número positivo \n";
+//		}
+//		else if (numero < 0) {
+//			cout << "O número negativo \n";
+//		}
+//		else {
+//			cout << "O número é zero \n";
+//		}
+//
+//	}
+//}
